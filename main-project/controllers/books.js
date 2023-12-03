943a1116cc54a357db07b911377eef3925ab9f7b
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-12-04 00:12:36 +0200

    start0412

--- main-project/controllers/books.js
+++ main-project/controllers/books.js
@@ -4,7 +4,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Book.find({owner}, "-createdAt -updatedAt");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Book.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
   res.json(result);
 };
 
